f4799f4e98ca0649b38a27aa00731e22db9e0ba0
bitoller <bi_toller@hotmail.com>
2024-05-08 14:55:35 -0300

    feat: add toastify

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { MainRoutes } from "./routes";
 import { GlobalStyle } from "./styles/GlobalStyles";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <>
       <GlobalStyle />
+      <ToastContainer position="top-right" theme="crimson" />
       <MainRoutes />
     </>
   );
