849b6448d9af2abd2a508c4ad582e34930b854b3
bitoller <bi_toller@hotmail.com>
2024-05-08 14:57:54 -0300

    feat: add finish button functionality

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,20 +1,28 @@
 import footerDots from "../../assets/footer-dots.svg";
 import footerDotsRight from "../../assets/footer-dots-right.svg";
-import { StyledFooter } from "./style";
 import { useNavigate } from "react-router-dom";
+import { StyledFooter } from "./style";
 
 export function Footer({ showBackButton, showFinishButton }) {
   const navigate = useNavigate();
+
   return (
     <StyledFooter>
-      <img src={footerDots} alt="ícone com vários pontos" />
+      <img src={footerDots} alt={"ícone com vários pontos"} />
       {showBackButton && (
         <button className="back-button" onClick={() => navigate(-1)}>
           Voltar
         </button>
       )}
-      {showFinishButton && <button className="finish-button">Finalizar</button>}
-      <img src={footerDotsRight} alt="ícone com vários pontos" />
+      {showFinishButton && (
+        <button
+          className="finish-button"
+          onClick={() => navigate("/order_summary")}
+        >
+          Finalizar
+        </button>
+      )}
+      <img src={footerDotsRight} alt={"ícone com vários pontos"} />
     </StyledFooter>
   );
 }
