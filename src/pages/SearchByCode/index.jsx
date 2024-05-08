60cc5ddc6ab5de8e50ab3ab74403409986970e7e
bitoller <bi_toller@hotmail.com>
2024-05-08 15:00:09 -0300

    feat: add search by code screen

--- src/pages/SearchByCode/index.jsx
+++ src/pages/SearchByCode/index.jsx
@@ -1,24 +1,92 @@
 import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
 import { SearchButton } from "../../components/SearchButton";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import { StyledSearchByCode } from "./style";
 
 export function SearchByCode() {
+  const [jwtToken, setJwtToken] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    login();
+  }, []);
+
+  const login = async () => {
+    axios
+      .post(
+        "https://sso-catalogo.redeancora.com.br/connect/token",
+        {
+          client_id: "65tvh6rvn4d7uer3hqqm2p8k2pvnm5wx",
+          client_secret: "9Gt2dBRFTUgunSeRPqEFxwNgAfjNUPLP5EBvXKCn",
+          grant_type: "client_credentials",
+        },
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        }
+      )
+      .then((response) => {
+        setJwtToken(response.data.access_token);
+        localStorage.setItem("token", response.data.access_token);
+      });
+  };
+
+  const search = async (event) => {
+    event.preventDefault();
+    const form = event.target;
+
+    if (!form.productInput.value) {
+      toast.warn("Por favor, insira o nome do produto");
+      return;
+    }
+
+    axios
+      .post(
+        "https://api-stg-catalogo.redeancora.com.br/superbusca/api/integracao/catalogo/v2/produtos/query/sumario",
+        {
+          nomeProduto: form.productInput.value,
+          superbusca: form.productInput.value,
+          pagina: 0,
+          itensPorPagina: 100,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((response) => {
+        localStorage.setItem("searchResult", JSON.stringify(response.data));
+        localStorage.setItem("lastSearch", form.productInput.value);
+        navigate("/search_results");
+      });
+  };
+
   return (
     <>
       <Header text="Pesquise pelo nome ou código do produto" />
       <StyledSearchByCode>
+        <h2>Digite abaixo o nome ou código do produto que precisa</h2>
         <section className="search-field-code">
-          <input
-            type="text"
-            id="produtoInput"
-            className="search-input"
-            placeholder="Digite o nome ou código do produto"
-          />
+          <form onSubmit={search}>
+            <input
+              type="text"
+              name="productInput"
+              className="search-input"
+              placeholder="Digite o nome ou código do produto"
+            />
+            <SearchButton type="submit" />
+          </form>
         </section>
-        <SearchButton />
       </StyledSearchByCode>
-      <Footer />
+      <Footer showBackButton />
+      {/* TODO: consertar CSS, consertar icone da lupa dentro do input */}
     </>
   );
 }
--- src/pages/SearchByCode/style.js
+++ src/pages/SearchByCode/style.js
@@ -1,15 +1,22 @@
 import styled from "styled-components";
 
 export const StyledSearchByCode = styled.main`
-  display: grid;
-  /* grid-template-rows: 1fr 3fr 5fr; */
+  display: flex;
   justify-items: center;
   min-height: calc(100vh - 120px - 120px);
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  gap: 50px;
+
+  h2 {
+    font-size: 36px;
+  }
 
   .search-field-code {
     display: flex;
     width: 100%;
-    height: 125px;
+    height: 450px;
     background-color: var(--color-gray-9);
     justify-content: center;
     align-items: center;
