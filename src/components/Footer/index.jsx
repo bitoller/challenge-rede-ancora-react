4206720791d29a74559791963cc710aeaf4507ae
bitoller <bi_toller@hotmail.com>
2024-05-08 00:29:30 -0300

    feat: add functioning return button

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,12 +1,18 @@
 import footerDots from "../../assets/footer-dots.svg";
 import footerDotsRight from "../../assets/footer-dots-right.svg";
 import { StyledFooter } from "./style";
+import { useNavigate } from "react-router-dom";
 
 export function Footer({ showBackButton, showFinishButton }) {
+  const navigate = useNavigate();
   return (
     <StyledFooter>
       <img src={footerDots} alt="ícone com vários pontos" />
-      {showBackButton && <button className="back-button">Voltar</button>}
+      {showBackButton && (
+        <button className="back-button" onClick={() => navigate(-1)}>
+          Voltar
+        </button>
+      )}
       {showFinishButton && <button className="finish-button">Finalizar</button>}
       <img src={footerDotsRight} alt="ícone com vários pontos" />
     </StyledFooter>
