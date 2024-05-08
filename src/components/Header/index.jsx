5aecf9fd46960288f2baa814ac0b95999b0ae912
bitoller <bi_toller@hotmail.com>
2024-05-08 14:58:13 -0300

    feat: add link to home on logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,16 @@
 import logo from "../../assets/logo.png";
 import headerDots from "../../assets/header-dots.svg";
+import { Link } from "react-router-dom";
 import { StyledHeader } from "./style";
 
 export function Header({ text }) {
   return (
     <StyledHeader>
-      <img src={logo} alt="logo rede ancora" />
+      <Link to="/home">
+        <img src={logo} alt={"logo rede ancora"} />
+      </Link>
       <h1>{text}</h1>
-      <img src={headerDots} alt="..." />
+      <img src={headerDots} alt={"ícone com vários pontos"} />
     </StyledHeader>
   );
 }
