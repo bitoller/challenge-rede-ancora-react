a23ec3c5aa3bc4e4d32a68fe0249403f3a5faa59
bitoller <bi_toller@hotmail.com>
2024-05-08 14:59:03 -0300

    feat: add search error style

--- src/components/PlateModal/style.js
+++ src/components/PlateModal/style.js
@@ -8,4 +8,13 @@ export const StyledPlateModal = styled.div`
   width: 23.063rem;
   height: auto;
   border-radius: 4px;
+
+  .search-plate-error {
+    display: none;
+    color: rgb(189, 34, 34);
+  }
+
+  .search-plate-error.error {
+    display: block;
+  }
 `;
