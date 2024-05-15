400ccbbcea912f2b6e845327aaebe35527ed842e
bitoller <bi_toller@hotmail.com>
2024-05-15 09:19:49 -0300

    fix: summary style

--- src/components/ItemInCartCard/index.jsx
+++ src/components/ItemInCartCard/index.jsx
@@ -1,4 +1,6 @@
 import imgDefault from "../../assets/default.jpg";
+import minusButton from "../../assets/lessButton.svg";
+import plusButton from "../../assets/moreButton.svg";
 import { StyledItemInCartCard } from "./style";
 
 export function ItemInCartCard({ product, onUpdateCart }) {
@@ -12,7 +14,7 @@ export function ItemInCartCard({ product, onUpdateCart }) {
 
   return (
     <StyledItemInCartCard>
-      <div>
+      <div className="img-container">
         <img
           src={`https://catalogopdtstorage.blob.core.windows.net/imagens-prd/produto/${product.imagemReal}`}
           alt={"Imagem do Produto"}
@@ -27,11 +29,21 @@ export function ItemInCartCard({ product, onUpdateCart }) {
         <p className="product-brand">Marca: {product.marca}</p>
         <p className="product-code">Código: {product.codigoReferencia}</p>
       </div>
-      <button onClick={handleDecrement}>-</button>
-      <p>{product.count}</p>
-      <button onClick={handleIncrement}>+</button>
-      <p className="product-price">R$ {product.price}</p>
-      <p className="product-price">R$ {product.price * product.count}</p>
+      <div className="counter-container">
+        <button onClick={handleDecrement}>
+          <img src={minusButton} alt={"botão de menos"} />
+        </button>
+        <p>{product.count}</p>
+        <button onClick={handleIncrement}>
+          <img src={plusButton} alt={"botão de mais"} />
+        </button>
+      </div>
+      <div className="price-container">
+        <p className="product-price-unit">Valor unitário: R$ {product.price}</p>
+        <p className="product-price-sum">
+          Valor total: R$ {product.price * product.count}
+        </p>
+      </div>
     </StyledItemInCartCard>
   );
 }
--- src/components/ItemInCartCard/style.js
+++ src/components/ItemInCartCard/style.js
@@ -3,22 +3,24 @@ import styled from "styled-components";
 export const StyledItemInCartCard = styled.li`
   display: flex;
   align-items: center;
+  justify-content: space-between;
   gap: 0.9375rem;
   width: 90%;
   height: 8.75rem;
+  padding: 10px;
   background-color: white;
   color: black;
   border: 1px solid black;
 
-  div {
+  .img-container {
     display: flex;
     justify-content: center;
     align-items: center;
     height: 8.5625rem;
-    width: 100%;
+    width: 300px;
   }
 
-  img {
+  .img-container > img {
     max-width: 100%;
     max-height: 100%;
     width: auto;
@@ -32,22 +34,30 @@ export const StyledItemInCartCard = styled.li`
     border-radius: 5px;
   }
 
-  p {
+  .product-name {
     font-size: 1.125rem;
     font-weight: 400;
-    width: 100%;
     font-family: var(--font-family-1);
     color: var(--color-primary);
     text-align: center;
   }
 
-  .product-name,
-  .product-price {
-    font-weight: 700;
-  }
-
   .brand-code-container {
     display: flex;
     flex-direction: column;
+  }
+
+  .counter-container {
+    display: flex;
+    gap: 15px;
+  }
+
+  .price-container {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .product-price-sum {
+    color: #c32401;
   }
 `;
